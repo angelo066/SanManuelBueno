57f06552f70255429fcbda4f853ca6ec419940ce
Angelo06 <angelo06@ucm.es>
2021-01-14 20:27:27 +0100

    Enemigo se hace pequeño

--- src/js/Enemigo.js
+++ src/js/Enemigo.js
@@ -29,7 +29,7 @@ export default class Enemigo extends Phaser.GameObjects.Sprite{
     this.scene.anims.create({
       key:'Boss_idle1',
       frames: this.scene.anims.generateFrameNumbers('Boss_Idle1',{start: 0, end: 7}),
-      frameRate: 0,
+      frameRate: 3,
       repeat: 0
     })
 
@@ -61,13 +61,15 @@ export default class Enemigo extends Phaser.GameObjects.Sprite{
       frameRate: 8,
       repeat: 0
     })
+    this.atacando = false;
   }
   
   Creapalabra()
   {
-    if(!this.fase)this.anims.play('Boss_attk1', true);
+    if(!this.fase){
+      this.anims.play('Boss_attk2', true);      //Animación del ataque quiero que se ejecute pero dice que no le apetece
+    }
     else this.anims.play('Boss_attk2', true);
-
     let palabra = new Proyectil(this.scene.matter.world,this.x + 50, this. y,'rosa', -4, 0, this);
 
     palabra.LanzaProyectil();
@@ -76,31 +78,30 @@ export default class Enemigo extends Phaser.GameObjects.Sprite{
   preUpdate(time, delta){
     super.preUpdate(time,delta);
 
-    if(!this.fase) this.anims.play('Boss_idle1',true);  //Ilde
-    else this.anims.play('Boss_idle2',true);
-
-    
+    //Compruebo si le toca atacar
     if(this.timer <= 0){
-      if(!this.fase){
-        this.anims.play('Boss_attk1', true);      //Animación del ataque quiero que se ejecute pero dice que no le apetece
-        console.log('Una chica una chica yeye que te compreeeeenda como yo');
-      }
-      else this.anims.play('Boss_attk2', true);
-
       this.Creapalabra();
       this.timer = this.tiempo;  
+      this.atacando = true;
     }
     else this.timer--;
 
+    //Compruebo si está modo panita
+    if(!this.fase && !this.atacando) this.anims.play('Boss_idle2',true);  //Ilde
+    else if(!this.atacando) this.anims.play('Boss_idle2',true);
+
+    if(this.anims.currentFrame.textureFrame === 7)this.atacando=false;
+
+    //Compruebo si se muere
     if(this.ActualWord.word === this.deadWord){
-      //Animación cuando la haya
+      //Animación y cambio de fase
       if(!this.fase)
       {
         this.anims.play('Boss_Death',true);
         this.fase = true;
       }
-
-      if(this.fase)this.destroy(true);
+      else this.destroy(true);
+     
     }
   }
 
