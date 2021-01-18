df3937e2ffdfd79c41d2cb214b186af807f9425b
Angelo06 <angelo06@ucm.es>
2021-01-18 19:20:13 +0100

    Los diálogos del final ya no son la letra de la chica yeye

--- src/js/endScene.js
+++ src/js/endScene.js
@@ -31,15 +31,10 @@ export default class Intro extends  Phaser.Scene {
     this.bg.setScale(1.5,1.5);
     this.bg.anims.play('opening', true);
     
-    this.dialogo = new Dialogue(this,["Que no te quieres enterar", "Yeye", "Que te quiero de verdad", "Yeye", "Y vendrás a pedirme de rodillas",
-    "Un poquiiiito de amor"]);
+    this.dialogo = new Dialogue(this,["Y así me llevé conmigo parte de su agonía", "Lo suficiente como para que me atormente personalmente",
+    "Y creo que me atormentará para siempre", "O bueno, hasta que muera", "Entonces, tampoco sufriré mucho"]);
     this.dialogo.onDialogue = true; //flag de activar dialogo
 
-    this.FadeIn();
-
-    this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
-      this.scene.start('level1');
-    });
   }
 
   FadeIn()
@@ -49,13 +44,5 @@ export default class Intro extends  Phaser.Scene {
     this.cameras.main.setBounds(0,0,7680, 2560);  
     
     this.cameras.main.zoomTo(0.8, 4000);
-  }
-
-  update(){
-    if(this.dialogo.endMessage === this.dialogo.i + 1 && !this.finished)
-    {
-      this.cameras.main.fadeOut(1000, 0, 0, 0);
-      this.finished = true;
-    }
   }
 }
